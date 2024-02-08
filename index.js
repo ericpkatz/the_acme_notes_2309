35af9b26ec3ef5e312de4327d6a53f6d08a0af26
EC2 Default User <ec2-user@ip-172-31-79-5.ec2.internal>
2024-02-08 16:26:44 +0000

    added post and delete routes

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const client = new pg.Client(process.env.DATABASE_URL || 'postgres://localhost/a
 const express = require('express');
 const app = express();
 const path = require('path');
-console.log(__dirname);
+
+app.use(express.json());
 
 app.get('/', (req, res, next)=> {
     res.sendFile(path.join(__dirname, 'index.html'));
@@ -17,6 +18,33 @@ app.get('/api/notes', async(req, res, next)=> {
         `;
         const response = await client.query(SQL);
         res.send(response.rows);
+    }
+    catch(ex){
+        next(ex);
+    }
+});
+
+app.post('/api/notes', async(req, res, next)=> {
+    try {
+        const SQL = `
+            INSERT INTO notes(txt) VALUES($1) RETURNING *
+        `;
+        const response = await client.query(SQL, [req.body.txt]);
+        res.status(201).send(response.rows[0]);
+    }
+    catch(ex){
+        next(ex);
+    }
+});
+
+app.delete('/api/notes/:id', async(req, res, next)=> {
+    try {
+        const SQL = `
+            DELETE FROM notes
+            WHERE id = $1
+        `;
+        await client.query(SQL, [req.params.id]);
+        res.sendStatus(204);
     }
     catch(ex){
         next(ex);
