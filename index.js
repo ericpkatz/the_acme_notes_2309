80ee4c69919ae1ce52e35adda2cad63be87768e9
EC2 Default User <ec2-user@ip-172-31-79-5.ec2.internal>
2024-02-08 15:14:55 +0000

    setup data layer

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-console.log('hello world!!');
+const pg = require('pg');
+const client = new pg.Client(process.env.DATABASE_URL || 'postgres://localhost/acme_notes_db');
+
+
+const init = async()=> {
+    await client.connect();
+    console.log('connected to database');
+    let SQL = `
+        DROP TABLE IF EXISTS notes;
+        CREATE TABLE notes(
+            id SERIAL PRIMARY KEY,
+            txt VARCHAR(100),
+            ranking INTEGER DEFAULT 5,
+            created_at TIMESTAMP DEFAULT now()
+        );
+    `;
+    await client.query(SQL);
+    console.log('tables created');
+    SQL = `
+        INSERT INTO notes(txt) VALUES ('learn SQL');
+        INSERT INTO notes(txt, ranking) VALUES ('learn Express', 10);
+        INSERT INTO notes(txt, ranking) VALUES ('learn to bake a cake', 1);
+    `;
+    await client.query(SQL);
+    console.log('data seeded');
+    
+};
+
+init();
